865eb6e27dcb30d995e571e2c3f24e272f5e096b
manuelgm92 <106698468+manuelgm92@users.noreply.github.com>
2026-09-08 19:28:36 +0100

    update main

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print("Screen width: 1280\nScreen height: 720")
+    pygame.init()
 
 if __name__ == "__main__":
     main()
+    
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+while True:
+    log_state()
+    
+    for event in pygame.event.get():
+        pass
+    
+    screen.fill("black")
+
+    pygame.display.flip()
