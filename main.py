56cb5392588a09836e5d2f34967900de4a1127d8
manuelgm92 <106698468+manuelgm92@users.noreply.github.com>
2026-09-09 13:52:00 +0100

    update constants, main and player

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
-    
     player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
     
     while True:
@@ -20,11 +19,16 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
+
         screen.fill("black")
+        player.update(dt)
+
         player.draw(screen)
         pygame.display.flip()
+        
+
         dt = clock.tick(60) / 1000
+        
         
 if __name__ == "__main__":
     main()
